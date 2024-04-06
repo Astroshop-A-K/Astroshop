22947149616f63b4283acd7942e56ea22dd913a8
Winlex1 <113421468+Winlex1@users.noreply.github.com>
2024-04-06 22:24:10 +0200

    Update order-page.component.ts

--- angular-project/src/app/order-page/order-page.component.ts
+++ angular-project/src/app/order-page/order-page.component.ts
@@ -13,4 +13,23 @@ export class OrderPageComponent {
   products = this.CartService.getProducts();
 
   constructor(private CartService: CartService){}
+
+  orderForm = new FormGroup({
+    name: new FormControl(''),
+    surname: new FormControl(''),
+    email: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    address: new FormControl(''),
+    psc: new FormControl(''),
+    city: new FormControl(''),
+    country: new FormControl(''),
+    deliveryOption: new FormControl(''),
+  });
+
+  onSelectChange(event: any){
+    this.orderForm.get('country')?.setValue(event.target.value);
+  }
+  onRadioChange(event: any){
+    this.orderForm.get('deliveryOption')?.setValue(event.target.value);
+  }
 }
