96b0d824d7f0252f20316b5ffad2bf2f640c1dc1
Winlex1 <113421468+Winlex1@users.noreply.github.com>
2024-04-14 16:45:18 +0200

    Update order-summary.component.ts

--- angular-project/src/app/order-summary/order-summary.component.ts
+++ angular-project/src/app/order-summary/order-summary.component.ts
@@ -1,24 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { OrderService } from '../order-page/order.service';
 import { NgFor } from '@angular/common';
 import { ProductsDTO } from '../products/products.component';
 import { CartService } from '../shopping-cart/cart.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-order-summary',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, ReactiveFormsModule, RouterLink],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.css'
 })
-export class OrderSummaryComponent implements OnInit{
+export class OrderSummaryComponent implements OnInit, OnDestroy{
   order: any[];
   selectedProducts: ProductsDTO[];
+  totalPrice = this.CartService.totalPrice();
 
-  constructor(private OrderService: OrderService, private CartService: CartService){}
+  constructor(private OrderService: OrderService, public CartService: CartService){}
+
+  paymentForm = new FormGroup({
+    paymentMethod: new FormControl('', Validators.required),
+  });
+
+  onRadioChange(event: any){
+    this.paymentForm.get('paymentMethod')?.setValue(event.target.value);
+  }
+
+  onSubmit(){
+    if(this.paymentForm.valid){
+      
+    }
+  }
 
   ngOnInit(): void {
     this.order = this.OrderService.order;
     this.selectedProducts = this.CartService.products;
+  }
+  ngOnDestroy(): void{
+    this.order = [];
+    this.OrderService.order = [];
   }
 }
