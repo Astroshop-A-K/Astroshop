f1c1427533bfb2e998530ae03399d138dedb0c57
Winlex1 <113421468+Winlex1@users.noreply.github.com>
2025-02-25 12:01:02 +0100

    Update products.component.ts

--- angular-project/src/app/products/products.component.ts
+++ angular-project/src/app/products/products.component.ts
@@ -78,6 +78,7 @@ export class ProductsComponent implements OnInit {
       this.updateCurrentProducts();
       this.selectedCategory = '';
       this.searchText = this.selectedSortOption = '';
+      this.isActive = false;
     }
   }
 
