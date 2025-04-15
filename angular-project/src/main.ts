838a479fba787b67ff6246e2bf07a628180be383
kacerika21 <kaalex822@gmail.com>
2025-04-15 08:27:21 +0200

    Forgot to change the URL.

--- angular-project/src/main.ts
+++ angular-project/src/main.ts
@@ -31,7 +31,7 @@ import { adminAuthGuard } from './app/api-authorization/admin-auth.guard';
 
 
 export function getBaseUrl() {
-  return 'https://localhost:7186/api/';
+  return 'https://astroshop.bsite.net/api/';
 }
 
 export function tokenGetter() {
